8ed13488661f50b8ed8dd8c986f8b84a3d8ba130
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-01-04 20:12:31 -0500

    configuration
    -- setting up tailwindcss
    -- importing tailwind scrollbar
    -- importing google fonts, icons
    -- removing extra files

--- keshav/tailwind.config.js
+++ keshav/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindScrollBar from 'tailwind-scrollbar';
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif']
+      }
+    },
   },
-  plugins: [],
+  plugins: [tailwindScrollBar],
 }
