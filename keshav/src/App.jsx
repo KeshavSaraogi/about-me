1293063758ad2617632c32897144b47c52ef6ee6
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-01-05 23:19:30 -0500

    nav items

--- keshav/src/App.jsx
+++ keshav/src/App.jsx
@@ -1,6 +1,36 @@
 import Header from './components/Header'
 
 const App = () => {
+
+  const navItems = [
+    {
+      label: 'Home',
+      link: '#home',
+      className: 'nav-link active',
+      ref: lastActiveLink
+    },
+    {
+      label: 'About',
+      link: '#about',
+      className: 'nav-link'
+    },
+    {
+      label: 'Work',
+      link: '#work',
+      className: 'nav-link'
+    },
+    {
+      label: 'Reviews',
+      link: '#reviews',
+      className: 'nav-link'
+    },
+    {
+      label: 'Contact',
+      link: '#contact',
+      className: 'nav-link md:hidden'
+    }
+  ];
+
   return (
     <Header />
   )
